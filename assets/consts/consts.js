d02efb6c251583438af0379a8965b63263a002a0
Adam <AdamJankowski5@gmail.com>
2022-04-30 15:06:22 +0200

    First commit. Data set up.

---
+++ assets/consts/consts.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+  golden: "FCC873",
+  black: "#030303",
+  white: "#FFFFFF",
+  darkgrey: "#282626",
+};
+export const FONTS = {
+  h1: { fontSize: 20, fontWeight: "700" },
+  h2: { fontSize: 16, fontWeight: "600" },
+  h3: { fontSize: 14, fontWeight: "500" },
+  h4: { fontSize: 12, fontWeight: "500" },
+};
---
+++ assets/data/WatchesData.js
@@ -0,0 +1,54 @@
+export const watches = [
+  {
+    collection: "Automata",
+    series: "BIRD REPEATER",
+    watchname: [
+      "BIRD REPEATER",
+      "BIRD REPEATER FALL OF THE RHINE",
+      "BIRD REPEATER OPENWORK",
+    ],
+    color: {},
+    image: [
+      require("../images/BIRD_REPEATER1resized.png"),
+      require("../images/BIRD_REPEATER2resized.png"),
+      require("../images/BIRD_REPEATER3resized.png"),
+    ],
+    price: 400,
+  },
+  {
+    collection: "GRANDE SECONDE",
+    series: "GRANDE SECONDE TOURBILLON",
+    watchname: [
+      "TOURBILLON CÔTES DE GENÈVE",
+      "TOURBILLON",
+      "TOURBILLON AVENTURINE",
+      "TOURBILLON IVORY ENAMEL",
+    ],
+    color: {},
+    image: [
+      require("../images/GRANDE_SECONDE_TOURBILLON1resized.png"),
+      require("../images/GRANDE_SECONDE_TOURBILLON2resized.png"),
+      require("../images/GRANDE_SECONDE_TOURBILLON3resized.png"),
+      require("../images/GRANDE_SECONDE_TOURBILLON4resized.png"),
+    ],
+    price: 600,
+  },
+  {
+    collection: "ASTRALE",
+    series: "PERPETUAL CALENDAR ÉCLIPSE",
+    watchname: ["PERPETUAL CALENDAR ÉCLIPSE IVORY ENAMEL"],
+    color: {},
+    image: [require("../images/ASTRALE_PERPETUAL_CALENDAR1resized.png")],
+    price: 450,
+  },
+  {
+    collection: "Ateliers D'art",
+    series: "THE ART OF PAILLONNÉ ENAMELING",
+    watchname: ["TOURBILLON PAILLONNÉE"],
+    color: {},
+    image: [require("../images/GRANDESECONDE1resized.png")],
+    price: 500,
+  },
+];
+
+export const watchSizes = [36, 41, 43, 46];
